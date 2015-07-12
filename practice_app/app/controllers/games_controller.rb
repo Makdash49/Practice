f92c226c9788afb828062376bf71e24a6fb91aff
Mark McQuillen <markdmcq@hotmail.com>
2015-07-12 12:46:04 -0700

    add container styling, random method, display random floor

--- practice_app/app/controllers/games_controller.rb
+++ practice_app/app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 class GamesController < ApplicationController
   def new
-  	"Welcome to the Egg Game!"
+  	# @game = Game.create(breakfloor: rand(1..100))
+  	@game = Game.create
+  	@game.update_attributes(breakfloor:@game.random_floor)
   end
 end
--- practice_app/app/models/game.rb
+++ practice_app/app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ActiveRecord::Base
+
+	def random_floor
+		rand(1..100)
+	end
+
 end
