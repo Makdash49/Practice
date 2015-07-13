0fa0f54313b325994950f98fc48b7eec1f7dfdd0
Mark McQuillen <markdmcq@hotmail.com>
2015-07-12 17:28:57 -0700

    Show newly created game id and breakfloor

--- practice_app/app/controllers/games_controller.rb
+++ practice_app/app/controllers/games_controller.rb
@@ -1,22 +1,34 @@
 class GamesController < ApplicationController
-  def new
-  	@game = Game.first
-  	@last_guess = Guess.last
-  	if @last_guess.floor == @game.breakfloor
-  		@message = "You got it right!!!"
-  	else
-  		@message = "Waiting for the correct guess..."
-  	end
+  # def new
+  # 	@game = Game.first
+  # 	@last_guess = Guess.last
+  # 	if @last_guess.floor == @game.breakfloor
+  # 		@message = "You got it right!!!"
+  # 	else
+  # 		@message = "Waiting for the correct guess..."
+  # 	end
 
-  	if @last_guess.floor >= @game.breakfloor
-  		@egg_message = "You broke the egg!"
-  		@game.update_attributes(eggs: @game.eggs - 1)
-  	else
-  		@egg_message = "The egg did not break"
-  	end
-  	# @game.update_attributes(breakfloor: Game.random_floor)
-  	@guess = @game.guesses.build
-  	@guess.save
+  # 	if @last_guess.floor >= @game.breakfloor
+  # 		@egg_message = "You broke the egg!"
+  # 		@game.update_attributes(eggs: @game.eggs - 1)
+  # 	else
+  # 		@egg_message = "The egg did not break"
+  # 	end
+  # 	# @game.update_attributes(breakfloor: Game.random_floor)
+  # 	@guess = @game.guesses.build
+  # 	@guess.save
+  # end
+
+  def new
+  end
+
+  def create
+  	@game = Game.create(breakfloor: Game.random_floor)
+    redirect_to @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
   end
 
 end
